88e840b34ea70b8a2a875de9d072cf465e5aa7bc
Andrzej Iwaniuk <andrzej.iwaniuk@live.co.uk>
2022-01-17 22:54:39 +0000

    added new controller product

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Add action logic here
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Details(int Id)
+        {
+
+            return View("Details");
+        }
+    }
+}
